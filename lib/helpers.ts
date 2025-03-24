3b710e55ecc5ffd47092b39b6cd08ce8932a86b9
k8pai <thek8pai@gmail.com>
2025-03-24 23:04:22 +0530

    update: included getMonths and substring helper functions

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -2,7 +2,9 @@ import {
     addDays,
     eachDayOfInterval,
     endOfMonth,
+    format,
     getDay,
+    set,
     startOfMonth,
     subDays,
 } from 'date-fns'
@@ -38,4 +40,26 @@ export const getSelectedMonthDays = (selectedDate: string) => {
             : []
 
     return [...daysInPreviousMonth, ...daysInCurrentMonth, ...daysInNextMonth]
+}
+
+export const getMonths = () => {
+    return Array.from({ length: 12 }, (_, i) =>
+        format(
+            set(new Date(), {
+                month: i,
+            }),
+            'MMMM'
+        )
+    )
+}
+
+export const isSubString = (str1: string, str2: string) => {
+    let condition = false
+
+    if (str1.length > str2.length) {
+        condition = str1.includes(str2)
+    } else {
+        condition = str2.includes(str1)
+    }
+    return condition
 }
