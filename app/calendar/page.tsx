8ba645f15c7c98732077f6bceeff623a88773f74
k8pai <thek8pai@gmail.com>
2025-03-27 23:36:14 +0530

    fix: included a new feature fix to show calendar views

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,11 +1,17 @@
 'use client'
 
 import Calendar from '@/components/calendar/Calendar'
+import Header from '@/components/calendar/Header'
 
 const CalendarPage = () => {
     return (
-        <div className="p-6">
-            <Calendar />
+        <div className="min-h-screen p-6 h-full flex flex-col">
+            <div>
+                <Header />
+            </div>
+            <div className="flex-1 flex flex-col">
+                <Calendar />
+            </div>
         </div>
     )
 }
