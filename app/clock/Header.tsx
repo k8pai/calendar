20eebc6ee79df0119a75dcf8e5191a3355b394f3
k8pai <thek8pai@gmail.com>
2025-04-09 23:18:00 +0530

    fix: included countries with timezones from IANA standards

--- app/clock/Header.tsx
+++ app/clock/Header.tsx
@@ -1,14 +1,21 @@
 'use client'
 
+import { ClockCommands } from '@/components/clock/ClockCommands'
 import { Separator } from '@/components/ui/separator'
 import { SidebarTrigger } from '@/components/ui/sidebar'
+import { getAllCountriesWithTimezones } from '@/lib/timezoneUtils'
 import { format } from 'date-fns'
 import { motion } from 'motion/react'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = ({}) => {
+    const countries = useMemo(() => {
+        let response = getAllCountriesWithTimezones()
+        return response
+    }, [])
+
     return (
         <div className="mb-4 flex justify-between items-center space-x-2">
             <motion.div className="flex-1 flex justify-start w-full items-center gap-1 px-2 lg:gap-2 lg:px-6">
@@ -17,6 +24,7 @@ const Header: React.FC<HeaderProps> = ({}) => {
                     orientation="vertical"
                     className="mx-2 data-[orientation=vertical]:h-4"
                 />
+                <ClockCommands countries={countries} />
             </motion.div>
 
             <motion.div className="flex-1 flex justify-end items-center gap-5">
