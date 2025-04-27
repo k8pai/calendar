9d5a953cbf0ddb48ba709b686c8ed55efe7ca404
k8pai <thek8pai@gmail.com>
2025-04-27 23:55:13 +0530

    fix: support for clock timezonelist

--- app/clock/page.tsx
+++ app/clock/page.tsx
@@ -1,17 +1,20 @@
 'use client'
 
-import Footer from '@/app/clock/Footer'
+import ClockOptions from '@/app/clock/ClockOptions'
 import Header from '@/app/clock/Header'
+import TimezoneList from '@/app/clock/TimezoneList'
 import Clock from '@/components/clock/Clock'
+import { useAppSelector } from '@/hooks/useTypedSelectors'
 
 const page = () => {
+    const { isInTimezoneView } = useAppSelector((state) => state.clock)
     return (
-        <div className="min-h-screen p-6 h-full flex flex-col">
+        <div className="min-h-screen  p-6 h-full flex flex-col">
             <Header />
-            <div className="flex-1 flex flex-col">
-                <Clock />
+            <div className="flex-1 flex flex-col relative">
+                <ClockOptions />
+                {isInTimezoneView ? <TimezoneList /> : <Clock />}
             </div>
-            <Footer />
         </div>
     )
 }
